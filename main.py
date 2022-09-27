8f3366384385ea65646abed902c7cc733f503866
Nick Reiter <nick.reiter@hotmail.de>
2022-09-27 22:14:40 +0200

    connected to ec2 instance and calculate

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+import boto3
+import botocore
+import paramiko
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    IP_ADRESS = '3.91.20.60'
+    KEY = paramiko.RSAKey.from_private_key_file('mykey.pem')
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Connect/ssh to an instance
+    # Here 'ubuntu' is user name and 'instance_ip' is public IP of EC2
+    client.connect(hostname= IP_ADRESS, username='ubuntu', pkey=KEY)
+
+    # Easy exemplary calculation
+    stdin, stdout, stderr = client.exec_command('echo "var=10;++var" | bc')
+    print('stdout:', stdout.read())
+    print('stderr:', stderr.read())
+
+    # close the client connection once the job is done
+    client.close()
+    print('TRY')
+
+    #except Exception:
+     #   print('EXCEPTION')
+
+
+
