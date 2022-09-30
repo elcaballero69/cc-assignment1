11881596ea0c41213a942052e0d626ededf7f05c
Nick Reiter <nick.reiter@hotmail.de>
2022-09-30 19:51:24 +0200

    added shell script to install Flask on an instance

--- flask.sh
+++ flask.sh
@@ -1,25 +1,28 @@
 # Do the necessary installations for flask
 sudo apt-get update
+# Install python, pip, flask, nginx and gunicorn3
 sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo pip3 install flask
-sudo apt-get install nginx
-sudo apt-get install gunicorn3
-mkdir flask_application
-cd flask_application
+sudo apt-get install python3 -V
+sudo apt-get install python3-venv
+mkdir flash_app && cd flask_app
+python3 -m venv venv
+source venv/bin/activate
+pip install flask
+vi flask_app.py
 
-# Make flask application
+
+
+# Make flask application - it is opened automatically
 # TODO give each instance a unique ID and include this in the return statement!
-at << EOF > flask_pythonscript.py
-#!/usr/bin/python
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
 
-EOF
+python3 flask_app.py
 
-chmod 755 flask_pythonscript.py
+change the security group to Custom TCP and port rangr 8080 and source 0.0.0.0/0
 
-./flask_pythonscript.py
