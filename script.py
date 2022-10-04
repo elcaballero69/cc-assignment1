a2f7208feae6f1fcc753209a0428bfee3580ee09
aurakiis <aura.kiiskinen@aalto.fi>
2022-10-03 20:18:54 -0400

    Instance creation
    Create 9 instances including security group

---
+++ script.py
@@ -0,0 +1,131 @@
+# Keys are defined in configuration file, do we need to do anything else??
+import boto3
+import json
+
+ec2_client = boto3.client("ec2")
+ec2 = boto3.resource('ec2')
+
+
+def createInstances(ec2, INSTANCE_TYPE, COUNT, SECURITY_GROUP, SUBNET_ID):
+    # Don't change these
+    KEY_NAME = "vockey"
+    INSTANCE_IMAGE = "ami-08d4ac5b634553e16"
+
+    return ec2.create_instances(
+        ImageId=INSTANCE_IMAGE,
+        MinCount=COUNT,
+        MaxCount=COUNT,
+        InstanceType=INSTANCE_TYPE,
+        KeyName=KEY_NAME,
+        SecurityGroupIds=SECURITY_GROUP,
+        SubnetId = SUBNET_ID
+    )
+
+def main(ec2_client, ec2):
+
+    # CODE TO CREATE INSTANCES STARTS HERE
+    # Creating 10 instances 
+
+    # Create security group, using only SSH access available from anywhere
+
+    groups = ec2_client.describe_security_groups()
+    vpc_id = groups["SecurityGroups"][0]["VpcId"]
+
+    new_group = ec2_client.create_security_group(
+        Description="SSH all",
+        GroupName="Cloud Computing TP1",
+        VpcId=vpc_id
+    )
+
+    group_id = new_group["GroupId"]
+
+    rule_creation = ec2_client.authorize_security_group_ingress(
+        GroupName="Cloud Computing TP1",
+        GroupId=group_id,
+        IpPermissions=[{
+            'FromPort': 22,
+            'ToPort': 22,
+            'IpProtocol': 'tcp',
+            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+        }]
+    )
+
+    SECURITY_GROUP = [group_id]
+
+    print(SECURITY_GROUP)
+
+    # Availability zones
+
+    response = ec2_client.describe_subnets()
+
+    availabilityzones = {}
+    for subnet in response.get('Subnets'):
+            #print(subnet)
+            availabilityzones.update({subnet.get('AvailabilityZone'): subnet.get('SubnetId')})
+
+    #print(availabilityzones)
+
+    # Get wanted availability zone
+    availability_zone_1a = availabilityzones.get('us-east-1a')
+    availability_zone_1b = availabilityzones.get('us-east-1b')
+    availability_zone_1c = availabilityzones.get('us-east-1c')
+
+    print(availability_zone_1a)
+
+    # Types: t2.large and m4.large, testing with micro
+
+    instances_t2_a = createInstances(ec2, "t2.large", 2, SECURITY_GROUP,availability_zone_1a)
+    instances_m4_a = createInstances(ec2, "m4.large", 2, SECURITY_GROUP,availability_zone_1a)
+    instances_t2_b = createInstances(ec2, "t2.large", 2, SECURITY_GROUP,availability_zone_1b)
+    instances_m4_b = createInstances(ec2, "m4.large", 2, SECURITY_GROUP,availability_zone_1b)
+    instances_t2_c = createInstances(ec2, "t2.large", 1, SECURITY_GROUP,availability_zone_1c)
+
+    print(instances_t2_a)
+
+    instance_ids = []
+
+    for instance in instances_t2_a:
+        instance_ids.append(instance.id)
+
+    for instance in instances_m4_a:
+        instance_ids.append(instance.id)
+
+    for instance in instances_t2_b:
+        instance_ids.append(instance.id)
+
+    for instance in instances_m4_b:
+        instance_ids.append(instance.id)
+
+    for instance in instances_t2_c:
+        instance_ids.append(instance.id)
+
+    print(instance_ids)
+
+
+def values(ec2_client, instance_ids):
+    
+    """
+    ------
+    THE INSTANCES ARE CREATED AS THEY SHOULD, HOWEVER WE ARE UNABLE TO GET THE DATA FOR THEM,
+    BECAUSE THEY ARE NOT FOUND YET, SCRIPT IS TOO FAST AND AWS TOO SLOW
+
+    HOWEVER, WE ARE NOT SURE IF WE ACTUALLY NEED THIS, DEPENDS ON HOW WE CAN CONNECT TO THE INSTANCES
+    ------
+    """
+
+    #  list of ids : [id,id,id]
+
+    instance_data_raw = ec2_client.describe_instances(InstanceIds=instance_ids)
+
+    # returns a list with a lot of paramaeters for each instance, PublicIpAddress parameter has the Public IPv4 address used for connection
+
+    instance_list = instance_data_raw["Reservations"][0]["Instances"]
+
+    print(instance_data_raw)
+
+    # Example Get IP address
+    print(instance_data_raw["Reservations"][0]["Instances"][0]["PublicIpAddress"])
+
+
+
+main(ec2_client, ec2)
