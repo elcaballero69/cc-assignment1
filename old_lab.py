995788ae8bc2668b0695b652c08c2c99d08e5d90
cedrichelewaut <cedric_helewaut@hotmail.com>
2022-10-17 09:14:03 -0400

    code cleaned up + servers fixed

--- old_lab.py
+++ old_lab.py
@@ -1,6 +1,4 @@
-# Keys are defined in configuration file
-# MAKE SURE YOU UPDATED YOUR .AWS/credentials file
-# MAKE SURE boto3, matplotlib, requests and tornado are all installed using pip
+# Keys are defined in configuration file, do we need to do anything else??
 import boto3
 import json
 import time
@@ -8,13 +6,8 @@ import subprocess
 import requests
 from multiprocessing import Pool
 from datetime import date
-from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import webbrowser
+from datetime import datetime
 
-# This makes the plots made by the script open in a webbrowser
-mpl.use('WebAgg')
 userdata="""#!/bin/bash
 cd /home/ubuntu
 mkdir flask_app
@@ -64,14 +57,12 @@ def createSecurityGroup(ec2_client):
             'ToPort': 22,
             'IpProtocol': 'tcp',
             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
-        },
-        {
+        },{
             'FromPort': 80,
             'ToPort': 80,
             'IpProtocol': 'tcp',
             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
-        },
-        {
+        },{
             'FromPort': 8080,
             'ToPort': 8080,
             'IpProtocol': 'tcp',
@@ -80,6 +71,8 @@ def createSecurityGroup(ec2_client):
     )
 
     SECURITY_GROUP = [group_id]
+    print("security_group: ", SECURITY_GROUP)
+
     return SECURITY_GROUP, vpc_id
 
 
@@ -110,25 +103,36 @@ def createInstance(ec2, INSTANCE_TYPE, COUNT, SECURITY_GROUP, SUBNET_ID):
         UserData=userdata
     )
 
+
 def createInstances(ec2_client, ec2, SECURITY_GROUP, availabilityZones):
+    # CODE TO CREATE INSTANCES STARTS HERE
+    # Creating 9 instances
+
     # Get wanted availability zone
     availability_zone_1a = availabilityZones.get('us-east-1a')
     availability_zone_1b = availabilityZones.get('us-east-1b')
     availability_zone_1c = availabilityZones.get('us-east-1c')
 
+    print("availability zone 1a: ", availability_zone_1a)
+    print("availability zone 1b: ", availability_zone_1b)
+    print("availability zone 1c: ", availability_zone_1c)
+
+    # Types: t2.large and m4.large use these for demo
+
+    # instances_t2_a = createInstance(ec2, "t2.large", 2, SECURITY_GROUP, availability_zone_1a)
+    # instances_m4_a = createInstance(ec2, "m4.large", 2, SECURITY_GROUP, availability_zone_1a)
+    # instances_t2_b = createInstance(ec2, "t2.large", 2, SECURITY_GROUP, availability_zone_1b)
+    # instances_m4_b = createInstance(ec2, "m4.large", 2, SECURITY_GROUP, availability_zone_1b)
+    # instances_t2_c = createInstance(ec2, "t2.large", 1, SECURITY_GROUP, availability_zone_1c)
+
     # Use this for development
-    # instances_t2_a = createInstance(ec2, "t2.micro", 2, SECURITY_GROUP, availability_zone_1a)
-    # instances_m4_a = createInstance(ec2, "t2.nano", 2, SECURITY_GROUP, availability_zone_1a)
-    # instances_t2_b = createInstance(ec2, "t2.micro", 2, SECURITY_GROUP, availability_zone_1b)
-    # instances_m4_b = createInstance(ec2, "t2.nano", 2, SECURITY_GROUP, availability_zone_1b)
-    # instances_t2_c = createInstance(ec2, "t2.micro", 1, SECURITY_GROUP, availability_zone_1c)
-    
-    # Use t2.large and m4.large for deployment/demo
-    instances_t2_a = createInstance(ec2, "t2.large", 2, SECURITY_GROUP, availability_zone_1a)
-    instances_m4_a = createInstance(ec2, "m4.large", 2, SECURITY_GROUP, availability_zone_1a)
-    instances_t2_b = createInstance(ec2, "t2.large", 2, SECURITY_GROUP, availability_zone_1b)
-    instances_m4_b = createInstance(ec2, "m4.large", 2, SECURITY_GROUP, availability_zone_1b)
-    instances_t2_c = createInstance(ec2, "t2.large", 1, SECURITY_GROUP, availability_zone_1c)
+    instances_t2_a = createInstance(ec2, "t2.micro", 2, SECURITY_GROUP, availability_zone_1a)
+    instances_m4_a = createInstance(ec2, "t2.nano", 2, SECURITY_GROUP, availability_zone_1a)
+    instances_t2_b = createInstance(ec2, "t2.micro", 2, SECURITY_GROUP, availability_zone_1b)
+    instances_m4_b = createInstance(ec2, "t2.nano", 2, SECURITY_GROUP, availability_zone_1b)
+    instances_t2_c = createInstance(ec2, "t2.micro", 1, SECURITY_GROUP, availability_zone_1c)
+
+    print("t2 instanses zone a: ", instances_t2_a)
 
     instance_ids = []
     T2_instance_ids = []
@@ -158,6 +162,86 @@ def createInstances(ec2_client, ec2, SECURITY_GROUP, availabilityZones):
     instance_running_waiter = ec2_client.get_waiter('instance_running')
     instance_running_waiter.wait(InstanceIds=(instance_ids))
 
+    print("T2_instance_ids", T2_instance_ids)
+    print("M4_instance_ids", M4_instance_ids)
+    print("instance_ids", instance_ids)
+
+    return instance_ids, T2_instance_ids, M4_instance_ids
+
+
+def createInstance2(ec2_client, ec2, INSTANCE_TYPE, COUNT, SECURITY_GROUP, SUBNET_ID):
+    # Don't change these
+    KEY_NAME = "vockey"
+    INSTANCE_IMAGE = "ami-08d4ac5b634553e16"
+
+    return ec2_client.run_instances(
+        ImageId=INSTANCE_IMAGE,
+        MinCount=COUNT,
+        MaxCount=COUNT,
+        Monitoring={'Enabled':True},
+        InstanceType=INSTANCE_TYPE,
+        KeyName=KEY_NAME,
+        SecurityGroupIds=SECURITY_GROUP,
+        SubnetId=SUBNET_ID,
+        UserData=userdata
+    )
+
+
+def createInstances2(ec2_client, ec2, SECURITY_GROUP, availabilityZones):
+    # CODE TO CREATE INSTANCES STARTS HERE
+    # Creating 9 instances
+
+    # Get wanted availability zone
+    availability_zone_1a = availabilityZones.get('us-east-1a')
+    availability_zone_1b = availabilityZones.get('us-east-1b')
+    availability_zone_1c = availabilityZones.get('us-east-1c')
+
+    print("availability zone 1a: ", availability_zone_1a)
+    print("availability zone 1b: ", availability_zone_1b)
+    print("availability zone 1c: ", availability_zone_1c)
+
+    # Types: t2.large and m4.large
+
+    instances_t2_a = createInstance(ec2_client, ec2, "t2.large", 2, SECURITY_GROUP, availability_zone_1a)
+    instances_m4_a = createInstance(ec2_client, ec2, "m4.large", 2, SECURITY_GROUP, availability_zone_1a)
+    instances_t2_b = createInstance(ec2_client, ec2, "t2.large", 2, SECURITY_GROUP, availability_zone_1b)
+    instances_m4_b = createInstance(ec2_client, ec2, "m4.large", 2, SECURITY_GROUP, availability_zone_1b)
+    instances_t2_c = createInstance(ec2_client, ec2, "t2.large", 1, SECURITY_GROUP, availability_zone_1c)
+
+    print("T2 instances for availability zone a: ", instances_t2_a)
+
+    instance_ids = []
+    T2_instance_ids = []
+    M4_instance_ids = []
+
+    for instance in instances_t2_a['Instances']:
+        instance_ids.append(instance['InstanceId'])
+        T2_instance_ids.append({'Id': instance['InstanceId']})
+
+    for instance in instances_m4_a['Instances']:
+        instance_ids.append(instance['InstanceId'])
+        M4_instance_ids.append({'Id': instance['InstanceId']})
+
+    for instance in instances_t2_b['Instances']:
+        instance_ids.append(instance['InstanceId'])
+        T2_instance_ids.append({'Id': instance['InstanceId']})
+
+    for instance in instances_m4_b['Instances']:
+        instance_ids.append(instance['InstanceId'])
+        M4_instance_ids.append({'Id': instance['InstanceId']})
+
+    for instance in instances_t2_c['Instances']:
+        instance_ids.append(instance['InstanceId'])
+        T2_instance_ids.append({'Id': instance['InstanceId']})
+
+    # Wait for all instances to be active!
+    instance_running_waiter = ec2_client.get_waiter('instance_running')
+    instance_running_waiter.wait(InstanceIds=(instance_ids))
+
+    print("T2_instance_ids", T2_instance_ids)
+    print("M4_instance_ids", M4_instance_ids)
+    print("instance_ids", instance_ids)
+
     return instance_ids, T2_instance_ids, M4_instance_ids
 
 def createTargetgroup(elbv2, vpc_id, name):
@@ -174,13 +258,20 @@ def createTargetGroups(elbv2, vpc_id):
     targetGroupT2 = createTargetgroup(elbv2, vpc_id, "cluster2")
     targetGroupM4 = createTargetgroup(elbv2, vpc_id, "cluster1")
 
+    print("Target group T2: ", targetGroupT2)
+    print("Target group M4: ", targetGroupM4)
+
     # get targetGroupARN
     ARN_T2 = targetGroupT2['TargetGroups'][0].get('TargetGroupArn')
     ARN_M4 = targetGroupM4['TargetGroups'][0].get('TargetGroupArn')
 
     return ARN_T2, ARN_M4
 
+
 def assignInstancesToTargetGroups(elbv2, ARN_T2, ARN_M4, T2_instance_ids, M4_instance_ids):
+    # assign instances to target groups
+    # wait for instances to be created properly
+    # time.sleep(25)
     targetgroupInstances_T2 = elbv2.register_targets(
         TargetGroupArn=ARN_T2,
         Targets=T2_instance_ids
@@ -192,9 +283,17 @@ def assignInstancesToTargetGroups(elbv2, ARN_T2, ARN_M4, T2_instance_ids, M4_ins
     health = elbv2.describe_target_groups(
         TargetGroupArns=[ARN_T2, ARN_M4]
     )
+
+    print("target group Instances_T2: \n", targetgroupInstances_T2)
+    print("target group Instances_M4: \n", targetgroupInstances_M4)
+    # For now only print
+    print("Health: \n", health)
     return targetgroupInstances_T2, targetgroupInstances_M4
 
+
 def createLoadBalancer(elbv2, SECURITY_GROUP, availabilityZones):
+    # will most probably need more arguments
+    # need to decide what typ of load balancer it should be
     loadBalancer=elbv2.create_load_balancer(
         Name="Lab1LoadBalancer",
         Subnets=[
@@ -208,18 +307,23 @@ def createLoadBalancer(elbv2, SECURITY_GROUP, availabilityZones):
         IpAddressType='ipv4'
     )
 
-    # print("Load Balancer: \n", loadBalancer)
+    print("Load Balancer: \n", loadBalancer)
     DNS_LB = loadBalancer['LoadBalancers'][0].get('DNSName')
     ARN_LB = loadBalancer['LoadBalancers'][0].get('LoadBalancerArn')
+    print("Load Balancer dns: ", DNS_LB)
+    print("Load Balancer ARN: ", ARN_LB)
     return DNS_LB, ARN_LB
 
+
+
+
 def assignTargetGroupsToLoadBalancer(elbv2, ARN_LB, ARN_T2, ARN_M4):
-    # Assign target groups to load balancer
+    # assign target groups to load balancer
+
     listener = elbv2.create_listener(
             LoadBalancerArn=ARN_LB,
             Port=80,
             Protocol='HTTP',
-            # Create default listener
             DefaultActions=[
                 {
                     'ForwardConfig': {
@@ -239,9 +343,12 @@ def assignTargetGroupsToLoadBalancer(elbv2, ARN_LB, ARN_T2, ARN_M4):
             ]
         )
 
-    # print("listener: ", listener)
+    print("listener: ", listener)
     ARN_Listener = listener['Listeners'][0].get('ListenerArn')
+    print("listener_ARN:", ARN_Listener)
+
     return ARN_Listener
+
 
 def make_rule(elbv2, ARN_Listener, ARN, priority, path):
     rule = elbv2.create_rule(
@@ -263,16 +370,12 @@ def make_rule(elbv2, ARN_Listener, ARN, priority, path):
             'TargetGroupArn': ARN}
         ]
     )
-    # print("rule:", rule)
-
+    print("rule:", rule)
     return rule
 
-def getCloudWatchMetrics(cw, startTime, ARN_targetgroup, name, ARN_LB):
+def getCloudWatchMetrics(cw, startTime, ARN_targetgroup):
     # check which metrics we want to retrieve
     # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-cloudwatch-metrics.html
-    print("Start Time:", startTime)
-    endTime = datetime.utcnow()
-    print("End Time:", endTime)
     data = cw.get_metric_data(
         MetricDataQueries=[
             {
@@ -280,146 +383,82 @@ def getCloudWatchMetrics(cw, startTime, ARN_targetgroup, name, ARN_LB):
                 'MetricStat': {
                     'Metric': {
                         'Namespace': 'AWS/ApplicationELB',
-                        'MetricName': 'UnHealthyHostCount',
+                        'MetricName': 'HealthyHostCount',
                         'Dimensions': [
                             {
                                 'Name': 'TargetGroup',
-                                'Value': ARN_targetgroup.split(':')[-1]
+                                'Value': 'string'
                             },
-                            {
-                                'Name': 'LoadBalancer',
-                                'Value': ARN_LB.split(':')[-1].split("loadbalancer/")[-1]
-                            }
                         ]
                     },
                     'Period': 1,
-                    'Stat': 'Average',
+                    'Stat': 'Minimum',
                 }
             },
         ],
         StartTime=startTime,
-        EndTime=endTime,
+        EndTime=datetime.now(),
         ScanBy='TimestampAscending',
     )
 
     print("cloudwatch data: ", data)
     return data
 
-def createPolicy(iam):
 
-    my_policy = {
-      "Version": "2012-10-17",
-      "Statement": [{
-          "Effect": "Allow",
-          "Action":
-              ["cloudwatch:GetMetricData"],
-          "Resource": "*"
-        }]
-    }
+def values(ec2_client, instance_ids):
+    instance_data_raw = ec2_client.describe_instances(InstanceIds=instance_ids)
+    instance_list = instance_data_raw["Reservations"][0]["Instances"]
 
-    policy = iam.create_policy(
-        PolicyName='CloudwatchPolicy',
-        PolicyDocument=json.dumps(my_policy)
-    )
+    # Example Get IP address
+    ins_ips = []
+    for res_id in range(len(instance_data_raw["Reservations"])):
+        for ins_id in range(len(instance_data_raw["Reservations"][res_id]["Instances"])):
+            ins_ips.append(instance_data_raw["Reservations"][res_id]["Instances"][ins_id]["PublicIpAddress"])
+    print("Instance IP adresses: ", ins_ips)
+    return ins_ips
 
-    # print("policy:", policy)
-    return policy
+# Functions to deploy flask
+
+def loop_subprocess(ins_ips):
+    counter = 0
+    for ins_ip in ins_ips:
+        counter += 1
+        subprocess.call(['sh', './lab1_flask.sh', ins_ip, str(counter)])
+        print(500 * "-")
+        print(str(ins_ip) + " has flask deployed!")
 
 def call_endpoint_http(DNS_LB, cluster):
     url = "http://"+ DNS_LB + "?cluster=" + cluster
     headers = {'content-type': 'application/json'}
     r = requests.get(url, headers=headers)
+    print(r.status_code)
     print(r.content)
 
-def plotData(data_t2, data_m4):
-    values_t2 = data_t2['MetricDataResults'][0].get('Values')
-    time_t2 = data_t2['MetricDataResults'][0].get('Timestamps')
-    print("Values:", values_t2)
-
-    values_m4 = data_m4['MetricDataResults'][0].get('Values')
-    time_m4 = data_m4['MetricDataResults'][0].get('Timestamps')
-    print("Values:", values_t2)
-
-    plt.subplot(1, 2, 1)
-    plt.plot(time_m4, values_m4)
-    plt.ylabel('Unhealhy Hosts')
-    plt.xlabel('Timestamp')
-    plt.xticks(rotation=45)
-    plt.yticks([0, 1, 2, 3, 4, 5, 6])
-    plt.title("Cluster 1, M4.Large")
-
-    plt.subplot(1, 2, 2)
-    plt.plot(time_t2, values_t2)
-    plt.ylabel('Unhealhy Hosts')
-    plt.xlabel('Timestamp')
-    plt.xticks(rotation=45)
-    plt.yticks([0, 1, 2, 3, 4, 5, 6])
-    plt.title("Cluster 2, T2.Large")
 
 
-    plt.show()
+
 
 def main():
-    # Get necesarry clients from boto3
     ec2_client = boto3.client("ec2")
     ec2 = boto3.resource('ec2')
     elbv2 = boto3.client('elbv2')
     cw = boto3.client('cloudwatch')
-    iam = boto3.client('iam')
-    startTime = datetime.utcnow()
-    # Create security group
+
+    startTime = datetime.now()
     SECURITY_GROUP, vpc_id = createSecurityGroup(ec2_client)
-    print("security_group: ", SECURITY_GROUP)
-    print("vpc_id: ", str(vpc_id), "\n")
-    # Create policy
-    policy = createPolicy(iam)
-    print("Cloud watch policy created \n")
-    # Get availability Zones
     availabilityZones = getAvailabilityZones(ec2_client)
-    print("Availability zones:")
-    print("Zone 1a: ", availabilityZones.get('us-east-1a'))
-    print("Zone 1b: ", availabilityZones.get('us-east-1b'))
-    print("Zone 1c: ", availabilityZones.get('us-east-1c'), "\n")
-    # Create the instances
     ins_ids, T2_instance_ids, M4_instance_ids = createInstances(ec2_client, ec2, SECURITY_GROUP, availabilityZones)
-    print("Instance ids: \n", str(ins_ids), "\n")
-    print("Instance ids T2: \n", str(T2_instance_ids), "\n")
-    print("Instance ids M42: \n", str(M4_instance_ids), "\n")
-    # Create the target groups
     ARN_T2, ARN_M4 = createTargetGroups(elbv2, vpc_id)
-    print("Target group T2 ARN: ", ARN_T2)
-    print("Target group M4 ARN: ", ARN_M4, "\n")
-    # Assign instances to the target groups
     targetgroupInstances_T2, targetgroupInstances_M4 = assignInstancesToTargetGroups(elbv2, ARN_T2, ARN_M4, T2_instance_ids, M4_instance_ids)
-    # Create the load balancer
     DNS_LB, ARN_LB = createLoadBalancer(elbv2, SECURITY_GROUP, availabilityZones)
-    print("Load Balancer DNS: ", DNS_LB)
-    print("Load Balancer ARN: ", ARN_LB, "\n")
-    # Create Listener
     ARN_Listener = assignTargetGroupsToLoadBalancer(elbv2, ARN_LB, ARN_T2, ARN_M4)
-    print("listener_ARN:", ARN_Listener, "\n")
-    # Add rules for cluster based routing
     make_rule(elbv2, ARN_Listener, ARN_T2, 1, 'cl2')
     make_rule(elbv2, ARN_Listener, ARN_M4, 2, 'cl1')
-    #wait until load balancer is available
-    print('WAITING FOR AVAILABILITY OF LOAD BALANCER...')
-    lb_waiter = elbv2.get_waiter('load_balancer_available')
-    lb_waiter.wait(LoadBalancerArns=[ARN_LB])
-    # Open up browser tab with the Loadbalancer
-    webbrowser.open(DNS_LB)
-    # Open up browser tabs for cluster based routing
-    webbrowser.open(DNS_LB + '?cluster=cl1')
-    webbrowser.open(DNS_LB + '?cluster=cl2')
-    # Request the flask server
-    print('REQUESTS ARE RUNNNING')
-    for i in range(0, 10):
-        print(f'REQUEST {i}')
-        call_endpoint_http(DNS_LB, 'cl1')
-        call_endpoint_http(DNS_LB, 'cl2')
-        time.sleep(5)
-    print('REQUESTS TERMINATED')
+    ins_ips = values(ec2_client, ins_ids)
+    #TODO: load balancer requires long initialization time. Thus, connection error
+    #call_endpoint_http(DNS_LB, 'cl1')
+    #call_endpoint_http(DNS_LB, 'cl2')
+    data_T2 = getCloudWatchMetrics(cw, startTime, ARN_T2)
+    data_M4 = getCloudWatchMetrics(cw, startTime, ARN_M4)
 
-    data_T2 = getCloudWatchMetrics(cw, startTime, ARN_T2, "cluster2", ARN_LB)
-    data_M4 = getCloudWatchMetrics(cw, startTime, ARN_M4, "cluster1", ARN_LB)
-    plotData(data_T2, data_M4)
 main()
